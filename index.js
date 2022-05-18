268f12985d8b48d90eee9fa27cbb17183e2d503e
cornelber <bercornel@gmail.com>
2022-05-18 19:24:05 +0300

    add functionality for currentCity and localStorage

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-displayCurrentWeather('București');
+const currentCityElement = document.querySelector('.current-city');
+let currentCity = localStorage.getItem('city');
+
+if(!currentCity) {
+    localStorage.setItem('city', 'București');
+    currentCity = 'București';
+} 
+
+updateWeather(currentCity);
