169fc7011ed4f8ad644caaa890cd65a9284d271e
cornelber <bercornel@gmail.com>
2022-05-18 19:27:15 +0300

    positioning changes

--- utils/date.js
+++ utils/date.js
@@ -32,18 +32,16 @@ const getDayOfTheWeek = (utc) => {
 };
 
 const getHour = (utc) => {
-    const date = new Date(utc * 1000);
-    
-    let hours = date.getHours();
-    if(hours < 10) {
-        hours = `0${hours}`;
-    }
+  const date = new Date(utc * 1000);
+  
+  let hours = date.getHours();
+  if(hours < 10) {
+      hours = `0${hours}`;
+  }
 
-    let minutes = date.getMinutes();
-    if(minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-    
-
-    return `${hours}:${minutes}`;
+  let minutes = date.getMinutes();
+  if(minutes < 10) {
+      minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
 };
